c272232d6fcbccbe8d86aed2eebe199494c3faef
ZhihaoJun <zhao11fs@gmail.com>
2017-10-11 15:04:34 +0800

    ready for test

--- README.md
+++ README.md
@@ -3,16 +3,15 @@
 
 **献给所有冰岩人，愿能为你的伟大创造添砖加瓦**
 
-
-
 ## 准备工作
+* 使用git做版本控制：[Installing Git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)
 * 开发、部署环境的配置都使用Docker：[Install Docker](https://docs.docker.com/engine/installation/)
 * 使用docker-compose，现在桌面操作系统上docker-compose已经和docker打包安装，无须独立安装，不过请确保已经安装，尤其是使用linux操作系统的：[Install Docker Compose](https://docs.docker.com/compose/install/)
-* 使用git做版本控制：[Installing Git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)
 * golang环境已经集成在docker容器中，包含以下库
   * `github.com/labstack/echo` 
+  * `github.com/utahta/echo-sessions`
+  * `github.com/boj/redistore`
   * `github.com/codegangsta/gin` 代码热更新工具
-  * `github.com/jmoiron/sqlx` 对原生库sql的扩展，提供数据库字段映射
 * 下载好预先制作好的docker镜像
   * `docker pull redis`
   * `docker pull postgres:9.4`
@@ -21,22 +20,16 @@
   * **如果pull的速度很慢，请尝试使用国内的docker镜像加速器来加速**
   * [镜像加速](https://www.docker-cn.com/registry-mirror)
 
-
-
-
 ## 快速开始
-
 1. clone这个仓库到本地 `git clone git@github.com:ZhihaoJun/GolangWebScaffold.git`
 2. 在项目根目录运行 `docker-compose up -d`
-3. 访问 [http://localhost:10060](http://localhost:10060/)
+3. 访问 [http://localhost:10060/?name=Bingyan](http://localhost:10060/?name=Bingyan) 看到 `Hello, Bingyan!`
+4. 访问 [http://localhost:10060/count](http://localhost:10060/count) 看到 ``
 4. 访问 [http://localhost:10061](http://localhost:10061) 进入数据库管理
    * 用户名 `postgres`
    * 密码为空
 
-
-
 ## 开发
-
 * 脚手架使用了gin来热更新加载代码，尝试修改[]()的代码，刷新网页可以看到即时更新的效果。
 * 阅读docker-compose.yml可以获得更多关于配置的信息
 * 已经内置了postgresql 9.4版本和redis最新版本
@@ -46,13 +39,7 @@
 * 版本管理，使用自己的仓库使用代码，务必重新 `git init`
 * golang库依赖都是在容器内部，为了让ide能够正确提示和补全代码，在本机上也需要安装相应的golang依赖库，推荐使用 [govendor](https://github.com/kardianos/govendor) 工具完成依赖库管理。
 
-
-
 ## 部署
-
 * [Docker Registry](https://docs.docker.com/registry/)
 
-
-
 ## 最佳实践
-
--- src/app/main.go
+++ src/app/main.go
@@ -1,31 +1,73 @@
 package main
 
 import (
-	"github.com/labstack/echo"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+
+	"github.com/boj/redistore"
+	"github.com/labstack/echo"
+	sessions "github.com/utahta/echo-sessions"
 )
 
 type App struct {
-	addr string
+	addr      string
+	secretKey string
 
-	e *echo.Echo
+	e          *echo.Echo
+	redisStore *redistore.RediStore
 }
 
-func (a *App) HelloWorld(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello World!")
+func (a *App) Hello(c echo.Context) error {
+	name := c.QueryParam("name")
+	return c.String(http.StatusOK, fmt.Sprintf("Hello %s!", name))
 }
 
-func (a *App) Setup() {
+func (a *App) SessionCount(c echo.Context) error {
+	countStr := sessions.GetRaw(c, "count").(string)
+	count, err := strconv.Atoi(countStr)
+	if err != nil {
+		count = 1
+	}
+	sessions.Set(c, "count", count)
+	if err := sessions.Save(c); err != nil {
+		return c.String(http.StatusInternalServerError, "Session save failed")
+	}
+	return c.String(http.StatusOK, fmt.Sprintf("Page has been visited %d times", count))
+}
+
+func (a *App) loadEnv() {
+	// ATTENTION: you should use your own prefix
 	a.addr = ":1323"
 	if v, ok := os.LookupEnv("APP_ADDR"); ok {
 		a.addr = v
 	}
+
+	// ATTENTION: this is default secre key
+	// you should change to a random one
+	a.secretKey = "secret-key"
+	if v, ok := os.LookupEnv("APP_SESSION_SECRET"); ok {
+		a.secretKey = v
+	}
+}
+
+func (a *App) Setup() {
+	a.loadEnv()
+	store, err := redistore.NewRediStore(10, "tcp", "redis:6379", "", []byte(a.secretKey))
+	if err != nil {
+		panic(err)
+	}
+	a.redisStore = store
+
 	a.e = echo.New()
+	// ATTENTION: you should use your own session id prefix
+	a.e.Use(sessions.Sessions("APP_SESS", store))
 
 	// setup routers
-	a.e.GET("/", a.HelloWorld)
+	a.e.GET("/", a.Hello)
+	a.e.GET("/count", a.SessionCount)
 }
 
 func (a *App) Run() {
@@ -33,7 +75,7 @@ func (a *App) Run() {
 }
 
 func (a *App) Close() {
-
+	a.redisStore.Close()
 }
 
 func main() {
